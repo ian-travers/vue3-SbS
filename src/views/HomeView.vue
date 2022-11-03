55ac24e0acecf222283eb6c0424b088a07c272cc
Ian Travers <mr.itpe@gmail.com>
2022-11-03 15:16:23 +0300

    Ep. 21 Refactor to defineProps and defineEmits (part 1: preparation)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,17 @@ const { flash } = useFlash();
 
 const food = useStorage("food");
 const age = useStorage("age");
+
+function onTabPress(e) {
+  const textarea = e.target;
+
+  const val = textarea.value;
+  const start = textarea.selectionStart;
+  const end = textarea.selectionEnd;
+
+  textarea.value = val.substring(0, start) + "\t" + val.substring(end);
+  textarea.selectionStart = textarea.selectionEnd = start + 1;
+}
 </script>
 
 <template>
@@ -15,7 +26,7 @@ const age = useStorage("age");
         Click Me
       </button>
     </div>
-    <div>
+    <div class="mt-6">
       <p>
         What is your favorite food?
         <input type="text" v-model="food" />
@@ -25,5 +36,17 @@ const age = useStorage("age");
         <input type="text" v-model="age" />
       </p>
     </div>
+    <div class="mt-6">
+      <textarea
+        @keydown.tab.prevent="onTabPress"
+        style="width: 100%; height: 250px"
+      >Hi there</textarea>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.mt-6 {
+  margin-top: 3rem;
+}
+</style>
