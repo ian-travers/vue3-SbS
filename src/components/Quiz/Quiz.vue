374572875433c6e69700f80d9e667516ac35673f
Ian Travers <mr.itpe@gmail.com>
2022-11-03 16:22:08 +0300

    Ep. 22 Dependency Injection With Provide and Inject (part 2: in action)

--- src/components/Quiz/Quiz.vue
+++ src/components/Quiz/Quiz.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { provide, ref } from "vue";
+
 import QuizHeader from "@/components/Quiz/QuizHeader.vue";
 import QuizQuestion from "@/components/Quiz/QuizQuestion.vue";
 import QuizFooter from "@/components/Quiz/QuizFooter.vue";
@@ -6,9 +8,14 @@ import QuizFooter from "@/components/Quiz/QuizFooter.vue";
 defineProps({
   quiz: Object,
 });
+
+const name = ref("John Doe");
+
+provide("name", name);
 </script>
 
 <template>
+  <h2>{{ name }}</h2>
   <QuizHeader />
   <QuizQuestion />
   <QuizFooter :quiz="quiz" />
--- src/components/Quiz/QuizFooterLinks.vue
+++ src/components/Quiz/QuizFooterLinks.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { inject } from "vue";
+
 defineProps({
   quiz: Object,
 });
+
+const name = inject("name");
+
+setTimeout(() => {
+  name.value = "McBride";
+}, 2500);
 </script>
 
 <template>
   <div>
-    <h5>{{ quiz.name }}</h5>
+    <h5>{{ name }}</h5>
     <ul>
       <li><a href="#">Get a Job</a></li>
       <li><a href="#">Contact Us</a></li>
