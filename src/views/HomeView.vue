97c8b8b653a237e40fde061264695a1b9e642e47
Ian Travers <mr.itpe@gmail.com>
2022-11-03 13:12:19 +0300

    Ep. 20 Composable Example: Local Storage (part 1: initial coding)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,34 @@
 <script setup>
+import { ref } from "vue";
+
 import { useFlash } from "@/composables/useFlash";
 
 const { flash } = useFlash();
+
+const food = ref(localStorage.getItem("food"));
+const age = ref(localStorage.getItem("age"));
+
+function write(key, val) {
+  localStorage.setItem(key, val);
+}
 </script>
 
 <template>
   <main>
-    <button @click="flash('It\'s Home', 'It Works!', 'info')">Click Me</button>
+    <div>
+      <button @click="flash('It\'s Home', 'It Works!', 'info')">
+        Click Me
+      </button>
+    </div>
+    <div>
+      <p>
+        What is your favorite food?
+        <input type="text" v-model="food" @input="write('food', food)" />
+      </p>
+      <p>
+        How old are you?
+        <input type="text" v-model="age" @input="write('age', age)" />
+      </p>
+    </div>
   </main>
 </template>
