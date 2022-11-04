9df122d286fa76cda3c4fe1b08bad2054f62c94c
Ian Travers <mr.itpe@gmail.com>
2022-11-04 15:55:59 +0300

    Ep. 26 Update json data

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -24,11 +24,11 @@
             d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
           />
         </svg>
-        <h3>Smiley Team</h3>
+        <h3>{{ team.name }} Team</h3>
         <div
-          class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
+          class="bg-green-700 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
         >
-          5
+          {{ team.spots }}
         </div>
       </div>
     </div>
@@ -46,7 +46,7 @@
 
       <tbody>
         <TeamMember
-          v-for="member in team"
+          v-for="member in team.members"
           :key="member.email"
           :name="member.name"
           :email="member.email"
@@ -61,7 +61,9 @@
   </div>
 
   <footer class="mt-12 bg-gray-100 py-4 text-center">
-    <h5 class="font-semibold text-lg">Smiley - 5 Member Team</h5>
+    <h5 class="font-semibold text-lg">
+      {{ team.name }} - {{ team.spots }} Member Team
+    </h5>
   </footer>
 </template>
 
