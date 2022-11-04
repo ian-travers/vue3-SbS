3297ec4bdcfab25cdb34bc349878ac3652845b52
Ian Travers <mr.itpe@gmail.com>
2022-11-04 16:23:55 +0300

    Ep. 26 Extract team members table (finish)

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,25 +2,7 @@
   <TeamHeader :team="team" />
 
   <div class="place-self-center flex flex-col gap-y-3 w-4/5 mx-auto">
-    <table class="table-fixed border-spacing-2 border-separate">
-      <thead>
-        <tr>
-          <th class="text-left px-6 py-2">Name</th>
-          <th class="text-left px-6 py-2">Email</th>
-          <th class="text-left px-6 py-2">Status</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <TeamMember
-          v-for="member in team.members"
-          :key="member.email"
-          :name="member.name"
-          :email="member.email"
-          :status="member.active"
-        />
-      </tbody>
-    </table>
+    <TeamMembers :team="team" />
 
     <p
       class="text-right text-gray-600 italic"
@@ -35,7 +17,7 @@
 
 <script setup>
 import TeamHeader from "@/components/Teams/TeamHeader.vue";
-import TeamMember from "@/components/Teams/TeamMember.vue";
-import team from "@/team.json";
+import TeamMembers from "@/components/Teams/TeamMembers.vue";
 import TeamFooter from "@/components/Teams/TeamFooter.vue";
+import team from "@/team.json";
 </script>
