81686cc9f7a9bc41ffc2a51e67660e8e93215354
Ian Travers <mr.itpe@gmail.com>
2022-11-04 16:17:24 +0300

    Ep. 26 Extract team header and footer

---
+++ src/components/Teams/TeamHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="flex justify-between">
+    <div>
+      <button
+        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+        :disabled="team.members.length === team.spots"
+      >
+        Add Member ({{ team.spots - team.members.length }} Spots Left)
+      </button>
+    </div>
+
+    <div>
+      <div class="inline-flex items-center text-3xl relative">
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 mr-2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+          />
+        </svg>
+        <h3>{{ team.name }} Team</h3>
+        <div
+          class="bg-green-700 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
+        >
+          {{ team.spots }}
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+defineProps({
+  team: Object,
+});
+</script>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,38 +1,5 @@
 <template>
-  <header class="flex justify-between">
-    <div>
-      <button
-        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
-        :disabled="team.members.length === team.spots"
-      >
-        Add Member ({{ team.spots - team.members.length }} Spots Left)
-      </button>
-    </div>
-
-    <div>
-      <div class="inline-flex items-center text-3xl relative">
-        <svg
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6 mr-2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-          />
-        </svg>
-        <h3>{{ team.name }} Team</h3>
-        <div
-          class="bg-green-700 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
-        >
-          {{ team.spots }}
-        </div>
-      </div>
-    </div>
-  </header>
+  <TeamHeader :team="team" />
 
   <div class="place-self-center flex flex-col gap-y-3 w-4/5 mx-auto">
     <table class="table-fixed border-spacing-2 border-separate">
@@ -63,14 +30,12 @@
     </p>
   </div>
 
-  <footer class="mt-12 bg-gray-100 py-4 text-center">
-    <h5 class="font-semibold text-lg">
-      {{ team.name }} - {{ team.spots }} Member Team
-    </h5>
-  </footer>
+  <TeamFooter :team="team" />
 </template>
 
 <script setup>
+import TeamHeader from "@/components/Teams/TeamHeader.vue";
 import TeamMember from "@/components/Teams/TeamMember.vue";
 import team from "@/team.json";
+import TeamFooter from "@/components/Teams/TeamFooter.vue";
 </script>
